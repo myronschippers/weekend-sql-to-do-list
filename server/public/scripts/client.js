d94892f534f1b9b353f2baf87ff3cd101b6bf440
Myron <dubtwee@gmail.com>
2019-05-06 10:12:38 -0500

    stub in JS functions needed to support the UI

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,3 +3,13 @@ $(document).ready(onReady);
 function onReady() {
     console.log('DOM is Ready. Start App');
 }
+
+function getTasks() {}
+
+function postTask() {}
+
+function putTask() {}
+
+function deleteTask() {}
+
+function render() {}
